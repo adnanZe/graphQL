0519c527f7e77c114980773fa7f39b4ff23c6b37
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2024-02-07 20:20:24 +0200

    [feature] configure apolloClient

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
-
+import {
+  ApolloProvider,
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from '@apollo/client';
 import App from './App';
 import './assets/css/index.css';
 
 import queryClient from './services/reactQueryClient';
 
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
