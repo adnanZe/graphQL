7fd62f7f1b7f44c5d85772d903fff0c561db2071
Zecheria, Adnan <adnan.zecheria@yahoo.com>
2024-02-24 18:44:58 +0200

    [wip] testing more mutations and updating store

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,15 +1,45 @@
+import { useCallback } from 'react';
+import AUTH_TOKEN from '../model/constants';
 import { Link as LinkModel } from '../model/link';
+import { timeDifferenceForDate } from '../services/time';
 
 interface Props {
   link: LinkModel;
+  index: number;
 }
 
-export default function Link({ link }: Props) {
+export default function Link({ link, index }: Props) {
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+
+  const handleVote = useCallback(() => {
+    console.log('Clicked vote button');
+  }, []);
+
   return (
-    <div>
-      <div>
-        {link.description}
-        {link.url}
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{index + 1}.</span>
+        {authToken && (
+          <div
+            className="ml1 gray f11"
+            style={{ cursor: 'pointer' }}
+            onClick={handleVote}
+          >
+            ▲
+          </div>
+        )}
+      </div>
+      <div className="ml1">
+        <div>
+          {link.description} ({link.url})
+        </div>
+        {
+          <div className="f6 lh-copy gray">
+            {link.votes.length} votes | by{' '}
+            {link.postedBy ? link.postedBy.name : 'Unknown'}{' '}
+            {timeDifferenceForDate(link.createdAt)}
+          </div>
+        }
       </div>
     </div>
   );
--- src/model/link.ts
+++ src/model/link.ts
@@ -3,4 +3,17 @@ export interface Link {
     id: string;
     description: string;
     url: string;
+    votes: Vote[];
+    postedBy: User;
+    createdAt: string;
+}
+
+export interface Vote {
+    id: string;
+    user: User;
+}
+
+export interface User {
+    id: string;
+    name: string;
 }
